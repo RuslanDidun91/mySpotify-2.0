d83ccaca193bac84db8cfde507cd3716f7ecb325
RuslanDidun <ruslandidun5@gmail.com>
2023-01-09 14:50:49 -0700

    TopCharts, AroundYou

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 const options = {
   method: 'GET',
   headers: {
-    'X-RapidAPI-Key': 'fc96a349admshcfacfe4928c8982p1de8e6jsn0ee11469fbfd',
+    'X-RapidAPI-Key': import.meta.env.VITE_SHAZAM_CORE_RAPID_API_KEY,
     'X-RapidAPI-Host': 'shazam-core.p.rapidapi.com'
   }
 };
@@ -29,7 +29,8 @@ export const shazamCoreApi = createApi({
     getTopCharts: builder.query({query: () => 'v1/charts/world'}),
     getSongDetails: builder.query({query: ({songid}) => `v1/tracks/details?track_id=${songid}`}),
     getSongRelated: builder.query({query:({songid}) => `v1/tracks/related?track_id=${songid}`}),
-    getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}` }),
+    getArtistDetails: builder.query({ query: (artistId) => `v2/artists/details?artist_id=${artistId}`}),
+    getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}`}),
   }),
 });
 
@@ -38,4 +39,5 @@ export const {
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
 } = shazamCoreApi
